99f6735a7d10fc4feca3ba18c86f549a728a22d2
Devin Tope <devintope@outlook.com>
2026-04-21 15:31:53 -0400

    deletion

--- nhl-fantasy/lib/api/routes.ts
+++
@@ -1,7 +0,0 @@
-export const apiRoutes = {
-  teams: "/api/teams",
-  players: "/api/players",
-  player: (playerId: number | string) => `/api/players/${playerId}`,
-  fantasyTeams: "/api/fantasy/teams",
-  fantasyTeamPlayers: (teamId: string) => `/api/fantasy/teams/${teamId}/players`,
-} as const;
